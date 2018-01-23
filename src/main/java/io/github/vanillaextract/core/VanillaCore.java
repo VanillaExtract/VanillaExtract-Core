d3c0fb140f1a7bce3670353ab84cc8e759b5e43b
0ct0ber <10884689+0ct0ber@users.noreply.github.com>
2018-01-23 09:15:18 -0800

    Update VanillaCore.java - Main class getter

--- src/main/java/io/github/vanillaextract/core/VanillaCore.java
+++ src/main/java/io/github/vanillaextract/core/VanillaCore.java
@@ -6,9 +6,12 @@ import org.bukkit.plugin.java.JavaPlugin;
  * Created by Synch on 2018-01-20.
  */
 public class VanillaCore extends JavaPlugin {
+    
+    private static VanillaCore main = null;
 
     @Override
     public void onEnable(){
+        main = this;
         //load disk operations, inject dependencies, etc
         
         getLogger().info("VanillaExtract (Core) v0.1-ALPHA enabled");
@@ -17,6 +20,17 @@ public class VanillaCore extends JavaPlugin {
     @Override
     public void onDisable(){
         //gc,flushing,saving,etc.
+        main = null;
+        
         getLogger().info("VanillaExtract (Core) v0.1-ALPHA disabled");
+    }
+    
+    public static VanillaCore getMainClass() {
+        if (!enabled()) throw new IllegalStateException("VanillaCore is disabled");
+        return main;
+    }
+    
+    public static boolean enabled() {
+        return main != null;
     }
 }
