c656f81d72fa1cefb4b7c4dea56187b74827505d
0ct0ber <10884689+0ct0ber@users.noreply.github.com>
2018-01-25 09:15:25 -0800

    Created MutableSoundValue.java

---
+++ src/main/java/io/github/vanillaextract/core/protocol_utils/sounds/MutableSoundValue.java
@@ -0,0 +1,27 @@
+/**A SoundValue object that allows for the editing of the sound, volume and pitch.
+  *
+  *{@inheritDoc}
+
+public class MutableSoundValue extends SoundValue {
+
+    public MutableSoundValue(Sound sound, float volume, float pitch) {
+        super(sound, volume, pitch);
+    }
+    
+    /**Sets the sound to the given sound value.
+      *@throws IllegalArgumentException If the sound provided is null
+      */
+    public final void setSound(Sound sound) throws IllegalArgumentException {
+    if (sound == null) throw new IllegalArgumentException("Sound cannot be null");
+    this.sound = sound;
+    }
+    
+    public final void setVolume(float volume) {
+    this.volume = volume;
+    }
+    
+    public final void setPitch(float pitch) {
+    this.pitch = pitch;
+    }
+
+}
