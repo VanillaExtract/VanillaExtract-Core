9bbc1ae3ca538c8a013f9fc55b9d1490effa07c6
0ct0ber <10884689+0ct0ber@users.noreply.github.com>
2018-01-25 08:49:54 -0800

    Create SoundValue.java
    Created this class, which is a convenient way to bundle sound values together. Also created an immutable SILENT SoundValue, which is wool breaking with a very quiet volume.

---
+++ src/main/java/io/github/vanillaextract/core/protocol_utils/sounds/SoundValue.java
@@ -0,0 +1,36 @@
+public class SoundValue {
+
+private Sound sound;
+private float volume;
+private float pitch;
+
+public static final SoundValue SILENT = new SoundValue(Sounds.trySounds("DIG_WOOL", "BLOCK_CLOTH_BREAK"), 0.0001f, 1f);
+
+
+/**Creates a trio of sound values with the given sound enum, volume, and pitch. This does not permit null values.
+  *
+  *@throws IllegalArgumentException If the sound provided is null
+  */
+public SoundValue(Sound sound, float volume, float pitch) {
+    if (sound == null) throw new IllegalArgumentException("Sound provided cannot be null");
+    this.sound = sound;
+    this.volume = volume;
+    this.pitch = pitch;
+    }
+    
+/**Returns the stored sound value. This should never return null.
+  *
+  */
+public Sound getSound() {
+    return Sound;
+    }
+    
+public float getVolume() {
+    return volume;
+    }
+
+public float getPitch() {
+    return pitch;
+    }
+
+}
