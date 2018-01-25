b9c7130235fc167238ee8d8c267bf0b61461428e
0ct0ber <10884689+0ct0ber@users.noreply.github.com>
2018-01-25 10:26:38 -0800

    Update InitializableListener.java

--- src/main/java/io/github/vanillaextract/core/utils/InitializableListener.java
+++ src/main/java/io/github/vanillaextract/core/utils/InitializableListener.java
@@ -1,3 +1,8 @@
+package io.github.vanillaextract.core.utils;
+
+import org.bukkit.plugin.Plugin;
+import org.bukkit.event.Listener;
+
 public interface InitializableListener<P extends Plugin> extends Listener, Initializable<P> {
 
     /**Runs any required initialization code before this listener is registered. If this method has
