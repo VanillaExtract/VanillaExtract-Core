85235e5376636e78bf97bf6c9b753afe40d8713b
0ct0ber <10884689+0ct0ber@users.noreply.github.com>
2018-01-25 09:40:45 -0800

    Update VanillaCore.java

--- src/main/java/io/github/vanillaextract/core/VanillaCore.java
+++ src/main/java/io/github/vanillaextract/core/VanillaCore.java
@@ -2,9 +2,7 @@ package io.github.vanillaextract.core;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-/**
- * Created by Synch on 2018-01-20.
- */
+
 public class VanillaCore extends JavaPlugin {
     
     private static VanillaCore main = null;
@@ -25,12 +23,17 @@ public class VanillaCore extends JavaPlugin {
         getLogger().info("VanillaExtract (Core) v0.1-ALPHA disabled");
     }
     
-    public static VanillaCore getMainClass() {
+    /**Gets the main class, if it is enabled. This will never return null, however it will throw an IllegalStateException
+      *when the plugin is disabled. A reference to the main class should ideally be kept once gotten.
+      *@return A reference to the main class object
+      *@throws IllegalStateException If the core is currently disabled
+      */
+    public static final VanillaCore getMainClass() throws IllegalStateException {
         if (!enabled()) throw new IllegalStateException("VanillaCore is disabled");
         return main;
     }
     
-    public static boolean enabled() {
+    public static final boolean enabled() {
         return main != null;
     }
 }
